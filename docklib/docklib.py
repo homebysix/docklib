f653a25ae314d5870ac47877cd27e2f6d9b9aa45
Allister Banks <arubdesu@me.com>
2024-04-04 21:38:27 +0900

    somehow smacked main docstring
    restore URL in docstring

--- docklib/docklib.py
+++ docklib/docklib.py
@@ -1,6 +1,9 @@
 # pylint: disable=C0103
 
-"""Python module intended to assist IT administrators with manipulation of the macOS Dock."""
+"""Python module intended to assist IT administrators with manipulation of the macOS Dock.
+
+See project details on GitHub: https://github.com/homebysix/docklib
+"""
 
 import os
 import subprocess
